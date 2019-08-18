1834f1d68afc66d24b33d1592146c191e0a78595
Im-Hul <blashirk@gmail.com>
2019-08-18 09:49:02 +0300

    RouterModule added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { HeaderComponent } from './header/header.component';
   imports: [
     BrowserModule,
     HttpClientModule, 
+    RouterModule,
   ],
   providers: [
     {
