703df5f9117d87f0763560f9ed7d79e57085f220
Im-Hul <blashirk@gmail.com>
2019-08-24 23:14:48 +0300

    rxjs: transformations, lesson 1 is done

--- src/app/rxjs/transform/transform.component.ts
+++ src/app/rxjs/transform/transform.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { pluck, reduce, scan } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-transform',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransformComponent implements OnInit {
 
+  // public observe = of(1, 2, 100, 14).pipe(reduce((accum, current) => accum + current)); // Работает как обычный reduce в js
+  // public observe = of({name: "John"}).pipe(pluck('name')); // Выводит заданное значените
+  public observe = of(1, 2, 100, 14).pipe(scan((accum, val) => accum + val)); // Работает как обычный reduce, но выводит значение на каждом промежуточном шаге суммирования
+
   constructor() { }
 
   ngOnInit() {
+    this.observe.subscribe({
+      next: (next: any) => console.info("range: next: ", next),
+      complete: () => console.info("range: Complete!"),
+      error: (error: any) => console.info("range: error: ", error),
+    });
   }
 
 }
