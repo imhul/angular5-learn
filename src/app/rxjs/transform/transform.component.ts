863c7bf425e7807a12dbc00d0ed22dd13e0225a8
Im-Hul <blashirk@gmail.com>
2019-08-24 23:41:15 +0300

    rxjs: transformations, lesson 2 is done

--- src/app/rxjs/transform/transform.component.ts
+++ src/app/rxjs/transform/transform.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { pluck, reduce, scan } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { 
+  pluck, 
+  reduce, 
+  scan, 
+  map, 
+  flatMap, 
+  switchMap, 
+  exhaust, 
+  exhaustMap,
+  concateMap,
+} from 'rxjs/operators';
+import { range, of, fromEvent, interval } from 'rxjs';
 
 @Component({
   selector: 'app-transform',
@@ -9,10 +19,20 @@ import { of } from 'rxjs';
 })
 export class TransformComponent implements OnInit {
 
-  // public observe = of(1, 2, 100, 14).pipe(reduce((accum, current) => accum + current)); // Работает как обычный reduce в js
   // public observe = of({name: "John"}).pipe(pluck('name')); // Выводит заданное значените
-  public observe = of(1, 2, 100, 14).pipe(scan((accum, val) => accum + val)); // Работает как обычный reduce, но выводит значение на каждом промежуточном шаге суммирования
+  // public observe = of(1, 2, 100, 14).pipe(reduce((accum, current) => accum + current)); // Работает как обычный reduce в js
+  // public observe = of(1, 2, 100, 14).pipe(scan((accum, val) => accum + val)); // Работает как обычный reduce, но выводит значение на каждом промежуточном шаге суммирования
 
+  // public observe = range(0, 100).pipe(map(item => item));
+  // mapTo - короткая запись метода map, когда все значения нужно поменять на другое
+
+  public clicks = fromEvent(document, 'click');
+  // public observe = this.clicks.pipe(flatMap(_ => interval(1000))); // flatMap === mergeMap, вмерживает параллельно
+  // concateMap вмерживает как flatMap, но последовательно
+  // public observe = this.clicks.pipe(switchMap(_ => interval(1000))); // Останавливает текущий таймер и запускает новый
+  public observe = this.clicks.pipe(exhaustMap(_ => interval(1000))); // Продолжает выполнять текущий таймер
+  
+  
   constructor() { }
 
   ngOnInit() {
